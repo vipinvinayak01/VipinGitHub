f0cc44ccf8bae313038b154988dbee51f69cd4b7
vipinvinayak <vipinvinayak01@gmail.com>
2017-01-15 21:15:40 -0800

    test for commit

--- VpinTestApplication/app/src/main/java/com/example/vpintestapplication/MainActivity.java
+++ VpinTestApplication/app/src/main/java/com/example/vpintestapplication/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //test code addded h
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
